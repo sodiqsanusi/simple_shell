954d27e44baf72c715d095adea40e2b050721487
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-11-15 17:58:14 +0000

    Task 4, and a bit of 3
    This might help boost our grades, or destroy everything we've worked on

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+static int cmd_count = 1;
+static int err_status = 1;
 /**
 * sighandler - Handles the signal from keyboard interrupts.
 * @sig: the signal caught.
@@ -10,6 +12,25 @@ static void sighandler(int sig)
 {
 	(void)sig;
 	write(STDOUT_FILENO, "\n", 1);
+}
+
+/**
+* handle_error - This is supposed to handle the error printing
+* @str: The file not found
+*
+* Return: "void" Nothing
+*/
+void handle_error(const char *str)
+{
+	if (_strncmp(str, "exit", 5) == 0)
+	{
+		if (cmd_count == 1)
+			errno = 0;
+		err_status = errno;
+		return;
+	}
+	dprintf(STDERR_FILENO, "./hsh: %d: %s: not found\n", cmd_count, str);
+	err_status = 127;
 }
 
 /**
@@ -27,6 +48,7 @@ int main(void)
 	while (1)
 	{
 		first_input = get_line();
+		errno = 0;
 		words = split_strings(first_input, " \t");
 		if (words == NULL)
 		{
@@ -36,28 +58,23 @@ int main(void)
 		checked_alias = get_path_and_clean(words[0]);
 		if (!checked_alias)
 		{
-			perror("file not found");
+			handle_error(words[0]);
 			free(first_input);
 			free_string_arr(words);
-			continue;
+			cmd_count++;
+			break;
 		}
 		free(words[0]);
 		words[0] = checked_alias;
 		child_pid = fork();
-		if (child_pid == -1)
-		{
-			perror("fork");
-			free(first_input);
-			free_string_arr(words);
-			continue;
-		}
 		if (words && child_pid == 0)
 			exec(words[0], words, environ);
 		else
 			wait(&status);
 		free(first_input);
 		free_string_arr(words);
+		cmd_count++;
 	}
-	return (0);
+	return (err_status);
 }
 
--- shell.h
+++ shell.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>

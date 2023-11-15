060cfecdd97ee5479fdd72624108ec3bc0ee3ba6
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-11-15 02:00:09 +0000

    Task 3, hopefully

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
-static unsigned char sig_flag = 1;
 /**
 * sighandler - handle signals from keyboard interrupts
 * @sig: the signal caught
 *
 * Return: "void" Nothing
 */
-static void sighandler(int sig __attribute__((unused)))
+static void sighandler(int sig)
 {
-	sig_flag = 0;
+	(void)sig;
+	write(STDOUT_FILENO, "\n$ ", 3);
 }
 
 /**
@@ -19,30 +19,30 @@ static void sighandler(int sig __attribute__((unused)))
 */
 int main(void)
 {
-	char *first_input = NULL;
-	char **words = NULL;
+	char *first_input = NULL, *checked_alias, **words = NULL;
 	pid_t child_pid;
 	int status;
 
-	if (signal(SIGINT, sighandler) == SIG_ERR)
-	{
-		perror("signal");
-		exit(98);
-	}
+	signal(SIGINT, sighandler);
 	while (1)
 	{
 		first_input = get_line();
-		if (sig_flag == 0)
-		{
-			free(first_input);
-			exit(98);
-		}
 		words = split_strings(first_input, " \t");
 		if (words == NULL)
 		{
 			free(first_input);
 			continue;
 		}
+		checked_alias = get_path_and_clean(words[0]);
+		if (!checked_alias)
+		{
+			perror("file not found");
+			free(first_input);
+			free_string_arr(words);
+			continue;
+		}
+		free(words[0]);
+		words[0] = checked_alias;
 		child_pid = fork();
 		if (child_pid == -1)
 		{
